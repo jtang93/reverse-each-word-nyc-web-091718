a48ed0e72f4c195202447a5cfe5081bdf510e7f8
jtang93 <tang.jonathan93@gmail.com>
2018-09-13 04:24:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   arr=string.split(" ")
-  rev=[]
+  rev = 
   arr.collect do |word|
-    rev << "#{word.reverse} "
+    rev << word.reverse
   end
+  rev.join(" ")
 end
